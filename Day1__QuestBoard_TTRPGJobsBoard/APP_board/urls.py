6c1b5badcd19e1d539e81015d1d1543b34cb0c1d
Daniel Cohen <danieljcoh@gmail.com>
2026-01-13 15:11:01 -0500

    fixed trailing /'s for URLs and changed template empty status line

--- Day1__QuestBoard_TTRPGJobsBoard/APP_board/urls.py
+++ Day1__QuestBoard_TTRPGJobsBoard/APP_board/urls.py
@@ -5,7 +5,7 @@ from .views import QuestBoardListView, QuestBoardDetailView, QuestBoardCreateVie
 urlpatterns = [
     path("", QuestBoardListView.as_view(), name="quest_list_view"),
     path("quest/<int:pk>/", QuestBoardDetailView.as_view(), name="quest_detail_view"),
-    path("quest/<int:pk>/update", QuestBoardUpdateView.as_view(), name="quest_update_view"),
-    path("quest/<int:pk>/delete", QuestBoardDeleteView.as_view(), name="quest_delete_view"),
+    path("quest/<int:pk>/update/", QuestBoardUpdateView.as_view(), name="quest_update_view"),
+    path("quest/<int:pk>/delete/", QuestBoardDeleteView.as_view(), name="quest_delete_view"),
     path("quest/new/", QuestBoardCreateView.as_view(), name="quest_new_view"),
 ]
